8d54a47f199f21611c5078b649c08a870292d22e
js0601 <selmeierjakob@gmail.com>
2024-04-28 14:35:08 +0200

    add a column counter to tokens

--- src/scanner.rs
+++ src/scanner.rs
@@ -6,25 +6,30 @@ pub fn scan(code: String) -> Vec<Token> {
     let mut lex_start = 0;
     let mut current_idx = 0;
     let mut line = 1;
+    let mut column = 1;
 
     while current_idx < code.len() {
         lex_start = current_idx;
-        match scan_token(&code, &mut lex_start, &mut current_idx, line) {
+        match scan_token(&code, &mut lex_start, &mut current_idx, line, column) {
             Ok(x) => match x {
                 // add token
                 Some(t) => {
-                    // if it's a EoL token increase the line counter
-                    if let TokenType::EndOfLine = t.token_type {
-                        line += 1;
-                    }
                     // move the index by the lexeme length
                     current_idx += t.value.len();
+                    // increase column counter by same amount
+                    column += t.value.len() as u64;
+                    // if it's a EoL token increase the line counter and reset column counter
+                    if let TokenType::EndOfLine = t.token_type {
+                        line += 1;
+                        column = 1;
+                    }
                     tokens.push(t)
                 }
                 // ignore
                 None => {
                     // ignored characters are always of length 1
                     current_idx += 1;
+                    column += 1;
                     continue;
                 }
             },
@@ -36,7 +41,7 @@ pub fn scan(code: String) -> Vec<Token> {
         }
     }
 
-    tokens.push(Token::create(TokenType::EndOfFile, line));
+    tokens.push(Token::create(TokenType::EndOfFile, line, column));
     tokens
 }
 
@@ -46,17 +51,18 @@ fn scan_token(
     lex_start: &mut usize,
     current_idx: &mut usize,
     line: u64,
+    column: u64,
 ) -> Result<Option<Token>, String> {
     let current_char = code
         .chars()
         .nth(*current_idx)
         .expect("This should not fail, since current_idx should not be out of bounds here");
     match current_char {
-        '+' => Ok(Some(Token::create(TokenType::Plus, line))),
-        '-' => Ok(Some(Token::create(TokenType::Minus, line))),
-        '*' => Ok(Some(Token::create(TokenType::Asterisk, line))),
-        '/' => Ok(Some(Token::create(TokenType::Slash, line))),
-        '\n' => Ok(Some(Token::create(TokenType::EndOfLine, line))),
+        '+' => Ok(Some(Token::create(TokenType::Plus, line, column))),
+        '-' => Ok(Some(Token::create(TokenType::Minus, line, column))),
+        '*' => Ok(Some(Token::create(TokenType::Asterisk, line, column))),
+        '/' => Ok(Some(Token::create(TokenType::Slash, line, column))),
+        '\n' => Ok(Some(Token::create(TokenType::EndOfLine, line, column))),
         // TODO: probably don't ignore all whitespace because of identation
         // TODO: check for all ignored chars if they work
         ' ' | '\r' => Ok(None),
--- src/token.rs
+++ src/token.rs
@@ -3,171 +3,204 @@ pub struct Token {
     pub token_type: TokenType,
     pub value: String,
     pub line: u64,
+    pub column: u64,
 }
 
 impl Token {
     // exists so scanner doesn't have to do it all
-    pub fn create(token_type: TokenType, line: u64) -> Self {
+    pub fn create(token_type: TokenType, line: u64, column: u64) -> Self {
         match token_type {
             TokenType::Plus => Self {
                 token_type,
                 value: "+".to_string(),
                 line,
+                column,
             },
             TokenType::Minus => Self {
                 token_type,
                 value: "-".to_string(),
                 line,
+                column,
             },
             TokenType::Asterisk => Self {
                 token_type,
                 value: "*".to_string(),
                 line,
+                column,
             },
             TokenType::Slash => Self {
                 token_type,
                 value: "/".to_string(),
                 line,
+                column,
             },
             TokenType::Colon => Self {
                 token_type,
                 value: ":".to_string(),
                 line,
+                column,
             },
             TokenType::LeftParen => Self {
                 token_type,
                 value: "(".to_string(),
                 line,
+                column,
             },
             TokenType::RightParen => Self {
                 token_type,
                 value: ")".to_string(),
                 line,
+                column,
             },
             TokenType::Hashtag => Self {
                 token_type,
                 value: "#".to_string(),
                 line,
+                column,
             },
             TokenType::NotEqual => Self {
                 token_type,
                 value: "!=".to_string(),
                 line,
+                column,
             },
             TokenType::Equal => Self {
                 token_type,
                 value: "=".to_string(),
                 line,
+                column,
             },
             TokenType::DoubleEqual => Self {
                 token_type,
                 value: "==".to_string(),
                 line,
+                column,
             },
             TokenType::Greater => Self {
                 token_type,
                 value: ">".to_string(),
                 line,
+                column,
             },
             TokenType::GreaterEqual => Self {
                 token_type,
                 value: ">=".to_string(),
                 line,
+                column,
             },
             TokenType::Less => Self {
                 token_type,
                 value: "<".to_string(),
                 line,
+                column,
             },
             TokenType::LessEqual => Self {
                 token_type,
                 value: "<=".to_string(),
                 line,
+                column,
             },
             TokenType::True => Self {
                 token_type,
                 value: "True".to_string(),
                 line,
+                column,
             },
             TokenType::False => Self {
                 token_type,
                 value: "False".to_string(),
                 line,
+                column,
             },
             TokenType::Not => Self {
                 token_type,
                 value: "not".to_string(),
                 line,
+                column,
             },
             TokenType::And => Self {
                 token_type,
                 value: "and".to_string(),
                 line,
+                column,
             },
             TokenType::Or => Self {
                 token_type,
                 value: "or".to_string(),
                 line,
+                column,
             },
             TokenType::If => Self {
                 token_type,
                 value: "if".to_string(),
                 line,
+                column,
             },
             TokenType::Else => Self {
                 token_type,
                 value: "else".to_string(),
                 line,
+                column,
             },
             TokenType::While => Self {
                 token_type,
                 value: "while".to_string(),
                 line,
+                column,
             },
             TokenType::Def => Self {
                 token_type,
                 value: "def".to_string(),
                 line,
+                column,
             },
             TokenType::Return => Self {
                 token_type,
                 value: "return".to_string(),
                 line,
+                column,
             },
             TokenType::None => Self {
                 token_type,
                 value: "None".to_string(),
                 line,
+                column,
             },
             TokenType::Identifier(ref n) => Self {
                 value: n.to_string(),
                 token_type,
                 line,
+                column,
             },
             TokenType::String(ref s) => Self {
                 value: s.to_string(),
                 token_type,
                 line,
+                column,
             },
             TokenType::Int(x) => Self {
                 token_type,
                 value: x.to_string(),
                 line,
+                column,
             },
             TokenType::Float(x) => Self {
                 token_type,
                 value: x.to_string(),
                 line,
+                column,
             },
             TokenType::EndOfLine => Self {
                 token_type,
                 value: "\n".to_string(),
                 line,
+                column,
             },
             TokenType::EndOfFile => Self {
                 token_type,
                 value: "".to_string(),
                 line,
+                column,
             },
         }
     }
