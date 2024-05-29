1a2bf3b21c471510170971f4c5921c37afbf62f6
js0601 <selmeierjakob@gmail.com>
2024-05-29 15:37:04 +0200

    add unary and primary, rewrite check_type

--- src/parser.rs
+++ src/parser.rs
@@ -22,12 +22,14 @@ impl Parser {
 
     // checks if current token has one of the types
     fn check_type(&self, types: Vec<TokenType>) -> bool {
-        for t in types {
-            if self.tokens[self.current_idx].token_type == t {
-                return true;
-            }
+        // not very pretty, but needed some way to eliminate value inside literal types
+        match &self.tokens[self.current_idx].token_type {
+            TokenType::Identifier(_) => types.iter().any(|t| matches!(t, TokenType::Identifier(_))),
+            TokenType::String(_) => types.iter().any(|t| matches!(t, TokenType::String(_))),
+            TokenType::Int(_) => types.iter().any(|t| matches!(t, TokenType::Int(_))),
+            TokenType::Float(_) => types.iter().any(|t| matches!(t, TokenType::Float(_))),
+            other => types.contains(other),
         }
-        false
     }
 
     // check_type but it moves the idx
@@ -122,11 +124,45 @@ impl Parser {
 
     // unary -> ("-"|"not") unary | primary
     fn unary(&mut self) -> Expr {
-        Expr::Literal(Lit::String("nothing".to_string()))
+        if self.check_advance(vec![TokenType::Minus, TokenType::Not]) {
+            // turn the token into a UnOp
+            let op = match self.tokens[self.current_idx - 1].token_type {
+                TokenType::Minus => UnOp::Minus,
+                TokenType::Not => UnOp::Not,
+                _ => panic!("In unary(): op token_type was not - or not, error probably in check_advance() or unary()"),
+            };
+            let right = self.unary();
+            return Expr::Unary(op, Box::new(right));
+        }
+        self.primary()
     }
 
     // primary -> NUMBER | STRING | "True" | "False" | "None" | "(" expr ")"
     fn primary(&mut self) -> Expr {
-        Expr::Literal(Lit::String("nothing".to_string()))
+        if self.check_advance(vec![
+            TokenType::String("".to_string()),
+            TokenType::Int(0),
+            TokenType::Float(0.0),
+        ]) {
+            match &self.tokens[self.current_idx - 1].token_type {
+                TokenType::String(s) => return Expr::Literal(Lit::String(s.to_string())),
+                TokenType::Int(n) => return Expr::Literal(Lit::Int(*n)),
+                TokenType::Float(n) => return Expr::Literal(Lit::Float(*n)),
+                _ => panic!("In primary(): op token_type was not String or Int or Float, error probably in check_literal_advance() or primary()"),
+            }
+        }
+
+        if self.check_advance(vec![TokenType::True]) {
+            return Expr::Literal(Lit::True);
+        }
+        if self.check_advance(vec![TokenType::False]) {
+            return Expr::Literal(Lit::False);
+        }
+        if self.check_advance(vec![TokenType::None]) {
+            return Expr::Literal(Lit::None);
+        }
+
+        // TODO: add grouping and error if no matching token was found
+        Expr::Literal(Lit::String("hehe".to_string()))
     }
 }
