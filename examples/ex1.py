52b84ff8d1f2b736d2f81853609fb0565d9b3014
js0601 <selmeierjakob@gmail.com>
2024-06-18 12:23:57 +0200

    change ex1.py to show variables

--- examples/ex1.py
+++ examples/ex1.py
@@ -1,5 +1,19 @@
+# declaration
 a = 5
-b = 2
-sum = a + b
+b = a * 1.5
+c = a + b
+print(a)
+print(b)
+print(c)
 
-print(sum)
+# reassignment
+a = 10 + 2 * 3
+print(a)
+
+# dynamic typing
+a = "abc"
+print(a)
+a = None
+print(a)
+a = 1.23
+print(a)
